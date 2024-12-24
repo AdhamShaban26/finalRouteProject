075787f4505003638b3a98c114f21d7270493818
AdhamShaban26 <adhamshaban791@gmail.com>
2024-12-24 21:22:18 +0200

    das

--- src/components/Payment/Payment.jsx
+++ src/components/Payment/Payment.jsx
@@ -218,7 +218,7 @@ export default function Payment() {
         {
           headers: { token },
           params: {
-            url: currentUrl,
+            url: "/",
           },
         }
       )
